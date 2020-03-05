ddb1805bffbff63e00992a59aba4b31ea7ecb031
Todd Geist <todd@geistinteractive.com>
2020-03-05 06:54:29 -0800

    Initialize project using Create React App

---
+++ src/Widget.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import defaultConfig from "./configuration.json";
+import Addon from "./components/Addon";
+import Configurator from "./components/Configuration";
+
+function Widget(initialProps) {
+  const Config = initialProps.Config;
+  if (!Config || Object.keys(Config).length < 1) {
+    initialProps = { ...initialProps, Config: defaultConfig };
+    window.__initialProps__ = initialProps;
+  }
+
+  if (initialProps.ShowConfig) return <Configurator {...initialProps} />;
+  return <Addon {...initialProps} />;
+}
+
+export default Widget;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+import { init } from "fmw-utils";
+
+import './themed-bootstrap.css'
+import "./index.css";
+
+import Widget from "./Widget";
+
+function BootWidget(props) {
+  ReactDOM.render(<Widget {...props} />, document.getElementById("root"));
+}
+
+init(BootWidget);
